011b97e44aaa3a15d6e7e36560ee497333a783f4
Nizam Yuseri <nizamyuseri@MacBook-Air-Nizam.local>
2026-02-09 15:19:31 +0800

    fix vite variables

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -3,10 +3,10 @@ import { ExtractedItem } from "../types";
 
 // Initialize Gemini Client
 // Using process.env.API_KEY as strictly required by guidelines.
-if (!import.meta.env.API_KEY) {
+if (!import.meta.env.VITE_GEMINI_API_KEY) {
   throw new Error("Missing Gemini API Key. Please checked VITE_GEMINI_API_KEY in the environment variables.");
 }
-const ai = new GoogleGenAI({ apiKey: import.meta.env.API_KEY });
+const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GEMINI_API_KEY });
 
 const modelId = "gemini-3-flash-preview";
 
