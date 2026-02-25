eb69a81d118101a618b0556372d818c0e71f2a93
Nizam Yuseri <nizamyuseri@MacBook-Air-Nizam.local>
2026-02-25 08:27:18 +0800

    fix name

--- App.tsx
+++ App.tsx
@@ -685,6 +685,7 @@ const App: React.FC = () => {
          .maybeSingle();
         //  return { prof, profError } as any;
     // });
+    console.log('prof: ', prof);
       profile = prof.data;
       profileError = profError;
 
