7bfbab2675deae6450154b6250110eba08ca7893
Nizam Yuseri <nizamyuseri@MacBook-Air-Nizam.local>
2026-03-30 11:37:46 +0800

    fix odoo login

--- services/odooApi.ts
+++ services/odooApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",       
+  baseURL: "https://app.snabbb.com/api",       
   withCredentials: true, 
   headers: {
     "Content-Type": "application/json",
