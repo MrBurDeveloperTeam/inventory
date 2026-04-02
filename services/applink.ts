e965f640a1825bc5991dd5bdaf8be0d1672ad0e2
Nizam Yuseri <nizamyuseri@MacBook-Air-Nizam.local>
2026-04-02 09:21:23 +0800

    fix login from the app itself

--- services/applink.ts
+++ services/applink.ts
@@ -1,9 +1,8 @@
-import axios from "axios";
 import { api } from "./api";
 
 const applink = async (param: any) => {
     try {
-        const {data} = await axios.post('https://inventory.snabbb.com/api/v1/sso/app_link', {
+        const {data} = await api.post('/v1/sso/app_link', {
                     "jsonrpc": "2.0",
                     "method": "call",
                     "params": {
