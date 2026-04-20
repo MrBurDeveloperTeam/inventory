a9fb60e49eddd8e333516d390461458bd2d63974
Nizam Yuseri <nizamyuseri@MacBook-Air-Nizam.local>
2026-04-20 11:15:58 +0800

    fix consume credit

--- services/snabbbApi.ts
+++ services/snabbbApi.ts
@@ -1,14 +1,13 @@
-import api from "./odooApi";
-
 export async function callSnabbbApi<T>(
   path: string,
   method: 'GET' | 'POST' = 'GET',
   body?: Record<string, any>
 ): Promise<T> {
-  const baseUrl = import.meta.env.VITE_ODOO_BASE_URL;
+  // const baseUrl = import.meta.env.VITE_ODOO_BASE_URL;
+  const baseUrl = 'https://semistiffly-largando-alane.ngrok-free.dev';
   const apiKey = import.meta.env.VITE_SNABBB_API_KEY;
 
-  const res = await api.post(`${path}`, {
+  const res = await fetch(`${baseUrl}${path}`, {
     method,
     headers: {
       'Content-Type': 'application/json',
@@ -17,8 +16,8 @@ export async function callSnabbbApi<T>(
     body: method === 'POST' ? JSON.stringify(body ?? {}) : undefined,
   });
 
-  const data = await res.data;
-  if (!data.ok || data.ok === false) {
+  const data = await res.json();
+  if (!res.ok || data.ok === false) {
     throw new Error(data.error || 'Snabbb API error');
   }
   return data as T;
