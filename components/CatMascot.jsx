2363b4afd0b477f9dad4ab8e6e7fd53e14960656
Nizam Yuseri <nizamyuseri@MacBook-Air-Nizam.local>
2026-07-16 16:08:58 +0800

    fix cat move with dbl click

--- components/CatMascot.jsx
+++ components/CatMascot.jsx
@@ -690,9 +690,9 @@ export default function CatMascot({ onCatClick, disabled = false }) {
       }, duration * 1000);
     };
 
-    document.addEventListener('click', handleGlobalClick);
+    document.addEventListener('dblclick', handleGlobalClick);
     return () => {
-      document.removeEventListener('click', handleGlobalClick);
+      document.removeEventListener('dblclick', handleGlobalClick);
       if (walkTimeoutRef.current) {
         clearTimeout(walkTimeoutRef.current);
       }
